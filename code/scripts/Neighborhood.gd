da45a067f87ba8c07b332ea0b7b698e07eadee78
Petra Baranski <info@progsource.de>
2021-01-29 20:59:43 +0100

    Added more layers

--- code/scripts/Neighborhood.gd
+++ code/scripts/Neighborhood.gd
@@ -4,5 +4,5 @@ extends Node2D
 func _ready():
 	var pet = GLOBAL.actor_factory.create_pet(GLOBAL.PetType.Dog0, Vector2(0,48))
 	$objects.add_child(pet)
-	var player = GLOBAL.actor_factory.create_berry(GLOBAL.is_char_female, Vector2(48, 0))
-	$objects.add_child(player)
+	var player = GLOBAL.actor_factory.create_berry(GLOBAL.is_char_female, Vector2(0, 0))
+	$player.add_child(player)

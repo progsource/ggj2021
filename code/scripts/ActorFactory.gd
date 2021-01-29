1660310d3f19a7bf061d6170b05639e763e9f09f
Petra Baranski <info@progsource.de>
2021-01-29 19:03:19 +0100

    Added Berries with animations to test scene

--- code/scripts/ActorFactory.gd
+++ code/scripts/ActorFactory.gd
@@ -1,13 +1,27 @@
 extends Reference
 
-var actor_packed = load("res://scenes/Pet.tscn")
+var pet_packed = load("res://scenes/Pet.tscn")
+var berry_packed = load("res://scenes/Berry.tscn")
 
 func create_pet(pet_type : int, pos : Vector2) -> Object:
-	var instance = actor_packed.instance()
+	var instance = pet_packed.instance()
 
 	instance.add_component(load("res://scripts/ActorAnimationComponent.gd").new())
 	instance.position = pos
 	instance.get_node("Sprite").texture = load("res://assets/gfx/%s" % GLOBAL.PetImageMap[pet_type])
 	instance.add_to_group("pets")
+
+	return instance
+
+func create_berry(is_female : bool, pos : Vector2) -> Object:
+	var instance = berry_packed.instance()
+
+	instance.add_component(load("res://scripts/ActorAnimationComponent.gd").new())
+	instance.position = pos
+	if is_female:
+		instance.get_node("Sprite").texture = load("res://assets/gfx/berry_fem.png")
+	else:
+		instance.get_node("Sprite").texture = load("res://assets/gfx/berry_guy.png")
+	instance.add_to_group("player")
 
 	return instance
--- code/scripts/Neighborhood.gd
+++ code/scripts/Neighborhood.gd
@@ -4,3 +4,5 @@ extends Node2D
 func _ready():
 	var pet = GLOBAL.actor_factory.create_pet(GLOBAL.PetType.Dog0, Vector2(0,48))
 	$objects.add_child(pet)
+	var player = GLOBAL.actor_factory.create_berry(false, Vector2(48, 0))
+	$objects.add_child(player)

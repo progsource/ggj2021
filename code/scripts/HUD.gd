296872d573eeb1ef9f194601b9ba6bb8c192cab6
teemitzitrone <no.face.at.facebook+github@gmail.com>
2021-01-30 16:46:22 +0100

    add game data

--- code/scripts/HUD.gd
+++ code/scripts/HUD.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
 func _ready():
-	pass
-
+	GLOBAL.game_data.time = 0
+	
+func _process(delta):
+	GLOBAL.game_data["time"] += delta
+	var seconds = fmod(GLOBAL.game_data.time, 60)
+	var minutes = fmod(GLOBAL.game_data.time, 3600) / 60
+	var str_elapsed = "%02d : %02d" % [minutes, seconds]
+	$TimeBox/Container/Value.text = str_elapsed
